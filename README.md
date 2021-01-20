79cac3f73220ca97a7a8d27006e2860f0d6805d0
David <dad9489@rit.edu>
2021-01-20 18:54:57 -0500

    Implemented light sync

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # PhilipsHueControl
+
+This is a script for controlling the Philips Hue light bulbs in my house. I have it hooked up to macros on my keyboard
+to control my lights with the touch of a button.
+
+In order to run this script, you must create a json file titled user.json that contains the following information:
+```
+{
+    "username": PHILIPS_HUE_USERNAME
+}
+```
+`PHILIPS_HUE_USERNAME` is the id of the user on your Philips Hue bridge. For more information about creating this
+user visit, https://developers.meethue.com/develop/get-started-2/.
+
+This program accepts two command line arguments: the light setting and the room. If no room is supplied, it will use the
+defined `DEFAULT_ROOM`.
+
+An example of running this program would look like:
+```
+python light_control.py read "Living Room"
+```
---
+++ light_control.py
@@ -0,0 +1,51 @@
+import json
+import requests
+import sys
+
+READ = {"on": True, "bri": 254, "hue": 8593, "sat": 121, "xy": [0.4452, 0.4068], "ct": 343, "colormode": "xy"}
+RELAX = {"on": True, "bri": 144, "hue": 8593, "sat": 121, "xy": [0.5018, 0.4152], "ct": 447, "colormode": "xy"}
+DIMMED = {"on": True, "bri": 84, "hue": 7676, "sat": 199, "xy": [0.561, 0.4042], "ct": 443, "colormode": "xy"}
+OFF = {"on": False}
+
+DEFAULT_ROOM = 'Davids Room'
+
+
+def get_base_url():
+    with open('user.json') as f:
+        user = json.load(f)
+    user = user['username']
+    try:
+        res = requests.get('https://discovery.meethue.com/')
+        res = json.loads(res.content)
+        ip = res[0]['internalipaddress']
+        return f"http://{ip}/api/{user}"
+    except Exception as e:
+        raise Exception(f"There was a problem reaching the Philips Hue bridge: {e}")
+
+
+def main():
+    base_url = get_base_url()
+    groups = json.loads(requests.get(base_url+"/groups").content)
+    action_str = sys.argv[1]
+    if len(sys.argv) > 2:
+        room = sys.argv[2]
+    else:
+        room = DEFAULT_ROOM
+    room_id = [x['name'] for x in groups.values()].index(room) + 1
+
+    if action_str == "read":
+        action = READ
+    elif action_str == "relax":
+        action = RELAX
+    elif action_str == "dimmed":
+        action = DIMMED
+    elif action_str == "off":
+        action = OFF
+    else:
+        raise TypeError(f"Invalid command argument '{action_str}'")
+    requests.put(base_url+f"/groups/{room_id}/action", data=json.dumps(action))
+    x = 1
+
+
+if __name__ == '__main__':
+    main()
